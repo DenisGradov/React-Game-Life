92fc210cf396bf45883fdbc0fc8445da7959e23b
MSPHelperSamp <155314413+MSPHelperSamp@users.noreply.github.com>
2024-04-16 18:44:05 +0200

    попытка фикса

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,10 +1,17 @@
 import animationFigures from "./animation";
 import staticFigures from "./static";
-import movingFigures from "./moving";
+import glider from "./moving/glider";
+import gun from "./moving/gun";
 
 const figures = [
   { figure: staticFigures, title: "Статические" },
   { figure: animationFigures, title: "Анимированные" },
-  { figure: movingFigures, title: "Двигающиеся" },
+  {
+    figure: [
+      { title: "Глайдер", object: glider },
+      { title: "Ружье", object: gun },
+    ],
+    title: "Двигающиеся",
+  },
 ];
 export default figures;
